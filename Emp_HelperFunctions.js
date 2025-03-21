bf3ccf42f81f5f4c0e5977112d9fed4217ef8096
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-21 11:48:28 +0530

    Added Emp_HelperFuntions for 7D

--- Emp_HelperFunctions.js
+++ Emp_HelperFunctions.js
@@ -67,3 +67,9 @@ let fullDayWageArr = mapDayWithWageArr.filter(fulltimeWage);
 console.log("UC7C – Daily Wage Filter When FullTime Wage Earned:");
 console.log(fullDayWageArr);
 
+// UC 7D – Find the first occurrence when Full Time Wage was earned using find function
+function findFulltimeWage(dailyWage) {
+    return dailyWage.includes("160");
+}
+console.log("UC 7D – First time Fulltime wage was earned on Day: " + 
+    mapDayWithWageArr.find(findFulltimeWage));
