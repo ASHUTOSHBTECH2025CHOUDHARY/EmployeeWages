38be9f2573975c242968e12671d855ecf0336001
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-21 15:18:44 +0530

    Added  Email Validation by Regex

---
+++ EMAIL_Validation5.js
@@ -0,0 +1,10 @@
+const emailRegex = /^abc([._,+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2})?$/;
+
+const testCases = [
+    "abc@bridgelabz.co",
+    "abc.xyz@bridgelabz.co.in"
+];
+
+testCases.forEach(email => {
+    console.log(`${email}: ${emailRegex.test(email)}`);
+});
